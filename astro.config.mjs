a5d9693391d2a95bbfd160622c3a716f839d0f9e
AgentitoE <agentee200@gmail.com>
2025-04-06 11:41:53 +0200

    Update Dependecies and ad MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [mdx()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
